51adedaa2c28c489e043797ab843957930e9320f
rifraf93 <rafaelbeltu@gmail.com>
2022-06-07 22:31:32 +0200

    sed i

--- bin/build-deno.sh
+++ bin/build-deno.sh
@@ -4,11 +4,6 @@ echo "> building Deno"
 deno_dir=./deno
 src_dir=./src
 
-sedi="-i"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  sedi="-i ''"
-fi
-
 # delete and recreate deno folder
 rm -rf $deno_dir || true
 mkdir $deno_dir
@@ -35,7 +30,11 @@ do
   if [[ -d $file_path ]]; then
     search_implied_index="import (.*) from (.*)\/$dir_item\";"
     replace_implied_index="import \1 from \2\/$dir_item\/index\";"
-    find $deno_dir -type f -exec sed $sedi -E "s/$search_implied_index/$replace_implied_index/g" {} \;
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      find $deno_dir -type f -exec sed -i '' -E "s/$search_implied_index/$replace_implied_index/g" {} \;
+    else 
+      find $deno_dir -type f -exec sed -i -E "s/$search_implied_index/$replace_implied_index/g" {} \;
+    fi
   fi
 done
 
@@ -54,17 +53,29 @@ find $deno_dir -name "*.test.ts" -type f -delete
 # place .ts in paths
 search_path="import (.*) from (.*)\";"
 replace_path="import \1 from \2.ts\";"
-find $deno_dir -type f -exec sed $sedi -E "s/$search_path/$replace_path/g" {} \;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find $deno_dir -type f -exec sed -i '' -E "s/$search_path/$replace_path/g" {} \;
+else 
+  find $deno_dir -type f -exec sed -i -E "s/$search_path/$replace_path/g" {} \;
+fi
 
 # correct ..ts with ./index.ts
 search_mistake="import (.*) from \"..ts\";"
 replace_mistake="import \1 from \".\/index.ts\";"
-find $deno_dir -type f -exec sed $sedi -E "s/$search_mistake/$replace_mistake/g" {} \;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find $deno_dir -type f -exec sed -i '' -E "s/$search_mistake/$replace_mistake/g" {} \;
+else 
+  find $deno_dir -type f -exec sed -i -E "s/$search_mistake/$replace_mistake/g" {} \;
+fi
 
 # correct types with types.ts (multiline problem)
 search_types="from (.*)types\";"
 replace_types="from \1types.ts\";"
-find $deno_dir -type f -exec sed $sedi -E "s/$search_types/$replace_types/g" {} \;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find $deno_dir -type f -exec sed -i '' -E "s/$search_types/$replace_types/g" {} \;
+else 
+  find $deno_dir -type f -exec sed -i -E "s/$search_types/$replace_types/g" {} \;
+fi
 
 # copy README file
 cp README.md $deno_dir
@@ -72,18 +83,34 @@ cp README.md $deno_dir
 # prepare README for Deno
 search_pg="import { Pool } from \"pg\";"
 replace_pg="import postgres from \"https:\/\/deno.land\/x\/postgresjs\/mod.js\";"
-sed -i $sedi "s/$search_pg/$replace_pg/g" "$deno_dir/README.md"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/$search_pg/$replace_pg/g" "$deno_dir/README.md"
+else 
+  sed -i "s/$search_pg/$replace_pg/g" "$deno_dir/README.md"
+fi
 
 search_refql="from \"refql\";"
 replace_refql="from \"https:\/\/deno.land\/x\/refql\/mod.ts\";"
-sed -i $sedi "s/$search_refql/$replace_refql/g" "$deno_dir/README.md"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/$search_refql/$replace_refql/g" "$deno_dir/README.md"
+else 
+  sed -i "s/$search_refql/$replace_refql/g" "$deno_dir/README.md"
+fi
 
 search_pool="const pool = new Pool ({"
 replace_pool="const pool = postgres ({"
-sed -i $sedi "s/$search_pool/$replace_pool/g" "$deno_dir/README.md"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/$search_pool/$replace_pool/g" "$deno_dir/README.md"
+else 
+  sed -i "s/$search_pool/$replace_pool/g" "$deno_dir/README.md"
+fi
 
 search_querier="pool.query (query, values).then (({ rows }) => rows);"
 replace_querier="pool.unsafe (query, values);"
-sed -i $sedi "s/$search_querier/$replace_querier/g" "$deno_dir/README.md"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/$search_querier/$replace_querier/g" "$deno_dir/README.md"
+else 
+  sed -i "s/$search_querier/$replace_querier/g" "$deno_dir/README.md"
+fi
 
 echo "> finished building Deno"
