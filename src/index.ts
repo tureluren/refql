e70e5e71d606b36b95a4d7e564be97460f08b726
Rafael Tureluren <rafael.tureluren@practimed.be>
2022-12-22 13:43:20 +0100

    export fix

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,11 @@ import RQLTag from "./RQLTag";
 import SQLTag from "./SQLTag";
 import sql from "./SQLTag/sql";
 import Table from "./Table";
-import belongsTo from "./Table/belongsTo";
-import belongsToMany from "./Table/belongsToMany";
-import hasMany from "./Table/hasMany";
-import hasOne from "./Table/hasOne";
 
 export * from "./nodes";
 export * from "./common/types";
 
 export {
-  belongsTo,
-  belongsToMany,
-  hasMany,
-  hasOne,
   RQLTag,
   sql,
   SQLTag,
