4407833dbfa556192e9d3b4bcf543c6aa92f6de3
Rafael Tureluren <rafael.tureluren@practimed.be>
2023-02-15 11:06:42 +0100

    3.0.18

--- deno/SQLTag/sql.ts
+++ deno/SQLTag/sql.ts
@@ -6,7 +6,7 @@ import { isASTNode } from "../nodes/ASTNode.ts";
 import RQLTag from "../RQLTag/index.ts";
 import Table from "../Table/index.ts";
 
-export const parse = <Params, Output, Box extends Boxes>(strings: TemplateStringsArray, variables: SQLTagVariable<Params, Output, Boxes>[]) => {
+export const parse = <Params, Output, Box extends Boxes = "Promise">(strings: TemplateStringsArray, variables: SQLTagVariable<Params, Output, Boxes>[]) => {
   const nodes = [] as ASTNode<Params, Output, Box>[];
 
   for (let [idx, string] of strings.entries ()) {
