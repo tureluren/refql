5b4e0bc7197d0f385666df5f62e327fa9bd661a3
Rafael Tureluren <rafael.tureluren@practimed.be>
2025-06-16 23:01:43 +0200

    make introspection more testable

--- src/introspection/index.ts
+++ src/introspection/index.ts
@@ -86,151 +86,138 @@ const footerJs = [
   "exports.getTables = getTables;"
 ];
 
-async function introspect(sql: typeof sqlX, options: RequiredRefQLOptions) {
-  const outputJs = `${outputDir}/index.js`;
-  const outputTs = `${outputDir}/index.d.ts`;
-  const originalJs = fs.readFileSync (outputJs, "utf-8");
-  const originalTs = fs.readFileSync (outputTs, "utf-8");
+export async function introspectPG(sql: typeof sqlX, options: RequiredRefQLOptions) {
+  let outputJs = headerJs.join ("\n");
+  let outputTs = headerTs.join ("\n");
 
-  try {
+  const schemas = await getTables (sql);
 
-    // header
-    await fs.outputFile (outputJs, headerJs.join ("\n"));
-    await fs.outputFile (outputTs, headerTs.join ("\n"));
+  const relationships = await getRelationships (sql).then (rels => rels.map (rel => ({
+    ...rel,
+    column_names: rel.column_names.replace (/^{|}$/g, "").split (","),
+    foreign_column_names: rel.foreign_column_names.replace (/^{|}$/g, "").split (",")
+  })));
 
-    const schemas = await getTables (sql);
+  const schemaMap = await Promise.all (
+    Object.keys (schemas).map (async schema => {
+      const tables = schemas[schema];
+      const interfacesNew = await Promise.all (
 
-    const relationships = await getRelationships (sql).then (rels => rels.map (rel => ({
-      ...rel,
-      column_names: rel.column_names.replace (/^{|}$/g, "").split (","),
-      foreign_column_names: rel.foreign_column_names.replace (/^{|}$/g, "").split (",")
-    })));
+        tables.map (async ({ table_name: table, table_schema }) => {
+          const tableName = toPascalCase (table);
+          const columns = await getColumns (sql, table);
+          const foreignKeys = relationships.filter (rel => rel.table_name === table);
+          const reversedForeignKeys = relationships.filter (rel => rel.foreign_table_name === table);
 
-    const schemaMap = await Promise.all (
-      Object.keys (schemas).map (async schema => {
-        const tables = schemas[schema];
-        const interfacesNew = await Promise.all (
+          const intermediateForeignKeys = relationships
+            .filter (rel => {
+              const table1 = options.toCase (`${table}_${rel.foreign_table_name}`);
+              const table2 = options.toCase (`${rel.foreign_table_name}_${table}`);
+              return rel.table_name === table1 || rel.table_name === table2;
+            })
+            .map (rRel => {
+              const lRel = relationships.find (rel => rel.table_name === rRel.table_name && rel.foreign_table_name === table);
+              return [lRel, rRel];
+            });
 
-          tables.map (async ({ table_name: table, table_schema }) => {
-            const tableName = toPascalCase (table);
-            const columns = await getColumns (sql, table);
-            const foreignKeys = relationships.filter (rel => rel.table_name === table);
-            const reversedForeignKeys = relationships.filter (rel => rel.foreign_table_name === table);
+          const props: {[propertyName: string]: string[]} = {};
 
-            const intermediateForeignKeys = relationships
-              .filter (rel => {
-                const table1 = options.toCase (`${table}_${rel.foreign_table_name}`);
-                const table2 = options.toCase (`${rel.foreign_table_name}_${table}`);
-                return rel.table_name === table1 || rel.table_name === table2;
-              })
-              .map (rRel => {
-                const lRel = relationships.find (rel => rel.table_name === rRel.table_name && rel.foreign_table_name === table);
-                return [lRel, rRel];
-              });
+          for (const col of columns) {
+            const propertyName = toCamelCase (col.column_name);
+            const nullable = col.is_nullable === "YES";
+            const hasDefault = col.column_default != null;
 
-            const props: {[propertyName: string]: string[]} = {};
+            const [propTypeJs, propTypeTs] = mapPostgresTypeToPropType (col.data_type, nullable);
 
-            for (const col of columns) {
-              const propertyName = toCamelCase (col.column_name);
-              const nullable = col.is_nullable === "YES";
-              const hasDefault = col.column_default != null;
-
-              const [propTypeJs, propTypeTs] = mapPostgresTypeToPropType (col.data_type, nullable);
-
-              props[propertyName] = [
-                `${propTypeJs} ("${propertyName}", "${col.column_name}")${hasDefault ? `.hasDefault ()` : nullable ? `.nullable ()` : ""}`,
-                `${propertyName}: Prop<"${propertyName}", ${propTypeTs}, {}, false, ${hasDefault ? "true" : "false"}, false>;`
-              ];
-            }
-
-
-            for (const fk of reversedForeignKeys) {
-              const lRef = fk.foreign_column_names;
-              const rRef = fk.column_names;
-
-              if (fk.unique) {
-                const propertyName = toCamelCase (fk.table_name);
-                props[propertyName] = [
-                  `(0, HasOne_1.default) ("${propertyName}", "${fk.table_schema}.${fk.table_name}", { lRef: ${refsAsString (lRef)}, rRef: ${refsAsString (rRef)} })`,
-                  `${propertyName}: RefProp<"${propertyName}", "${fk.table_schema}.${fk.table_name}", "HasOne", false>;`
-                ];
-
-              } else {
-                const propertyName = pluralize (toCamelCase (fk.table_name));
-                props[propertyName] = [
-                  `(0, HasMany_1.default) ("${propertyName}", "${fk.table_schema}.${fk.table_name}", { lRef: ${refsAsString (lRef)}, rRef: ${refsAsString (rRef)} })`,
-                  `${propertyName}: RefProp<"${propertyName}", "${fk.table_schema}.${fk.table_name}", "HasMany", false>;`
-                ];
-              }
-            }
-
-            // after reversedKeys to overwrite possible false HasOne's
-            for (const fk of foreignKeys) {
-              const lRef = fk.column_names;
-              const rRef = fk.foreign_column_names;
-
-              const propertyName = toCamelCase (getRelationKey (fk.foreign_table_name, lRef));
-
-              const cols = columns.filter (c => lRef.includes (c.column_name));
-
-              if (cols.length === 0) continue;
-
-              const nullable = cols.reduce ((acc, col) => acc || col.is_nullable === "YES", false) ;
-
-              props[propertyName] = [
-                `(0, BelongsTo_1.default) ("${propertyName}", "${fk.foreign_table_schema}.${fk.foreign_table_name}", { lRef: ${refsAsString (lRef)}, rRef: ${refsAsString (rRef)} })${nullable ? `.nullable ()` : ""}`,
-                `${propertyName}: RefProp<"${propertyName}", "${fk.foreign_table_schema}.${fk.foreign_table_name}", "BelongsTo", ${nullable ? "true" : "false"}>;`
-              ];
-            }
-
-            for (const fk of intermediateForeignKeys) {
-              const [lRel, rRel] = fk;
-              if (!lRel || !rRel) continue;
-              const lRef = lRel.foreign_column_names;
-              const lxRef = lRel.column_names;
-              const xTable = `${lRel.table_schema}.${lRel.table_name}`;
-              const rxRef = rRel.column_names;
-              const rRef = rRel.foreign_column_names;
-
-              const propertyName = pluralize (toCamelCase (rRel.foreign_table_name));
-
-              props[propertyName] = [
-                `(0, BelongsToMany_1.default) ("${propertyName}", "${rRel.foreign_table_schema}.${rRel.foreign_table_name}", { lRef: ${refsAsString (lRef)}, lxRef: ${refsAsString (lxRef)}, xTable: "${xTable}", rxRef: ${refsAsString (rxRef)}, rRef: ${refsAsString (rRef)} })`,
-                `${propertyName}: RefProp<"${propertyName}", "${rRel.foreign_table_schema}.${rRel.foreign_table_name}", "BelongsToMany", false>;`
-              ];
-            }
-
-            const interfaceProperties = Object.values (props);
-
-            return [
-              `      ${tableName}: Table ("${table_schema}.${table}", [\n        ${interfaceProperties.map (([js]) => js).join (",\n        ")}\n      ])`,
-              `    ${tableName}: Table<"${table_schema}.${table}", {\n      ${interfaceProperties.map (([_js, ts]) => ts).join ("\n      ")}\n    }>;`
+            props[propertyName] = [
+              `${propTypeJs} ("${propertyName}", "${col.column_name}")${hasDefault ? `.hasDefault ()` : nullable ? `.nullable ()` : ""}`,
+              `${propertyName}: Prop<"${propertyName}", ${propTypeTs}, {}, false, ${hasDefault ? "true" : "false"}, false>;`
             ];
-          }));
+          }
 
-        const schemaContent = [
-          `    ${schema}: {\n${interfacesNew.map (([js]) => js).join (",\n")}\n    }`,
-          `  ${schema}: {\n${interfacesNew.map (([_js, ts]) => ts).join ("\n")}\n  };`
-        ];
 
-        return schemaContent;
-      })
-    );
+          for (const fk of reversedForeignKeys) {
+            const lRef = fk.foreign_column_names;
+            const rRef = fk.column_names;
 
-    await fs.appendFile (outputJs, schemaMap.map (([js]) => js).join (",\n"));
-    await fs.appendFile (outputTs, schemaMap.map (([_js, ts]) => ts).join ("\n"));
+            if (fk.unique) {
+              const propertyName = toCamelCase (fk.table_name);
+              props[propertyName] = [
+                `(0, HasOne_1.default) ("${propertyName}", "${fk.table_schema}.${fk.table_name}", { lRef: ${refsAsString (lRef)}, rRef: ${refsAsString (rRef)} })`,
+                `${propertyName}: RefProp<"${propertyName}", "${fk.table_schema}.${fk.table_name}", "HasOne", false>;`
+              ];
 
-    // footer
-    await fs.appendFile (outputJs, footerJs.join ("\n"));
-    await fs.appendFile (outputTs, "\n};");
+            } else {
+              const propertyName = pluralize (toCamelCase (fk.table_name));
+              props[propertyName] = [
+                `(0, HasMany_1.default) ("${propertyName}", "${fk.table_schema}.${fk.table_name}", { lRef: ${refsAsString (lRef)}, rRef: ${refsAsString (rRef)} })`,
+                `${propertyName}: RefProp<"${propertyName}", "${fk.table_schema}.${fk.table_name}", "HasMany", false>;`
+              ];
+            }
+          }
 
-    console.log ("RefQL introspect completed!");
-  } catch (e) {
-    console.error (`RefQL introspect failed!`);
+          // after reversedKeys to overwrite possible false HasOne's
+          for (const fk of foreignKeys) {
+            const lRef = fk.column_names;
+            const rRef = fk.foreign_column_names;
 
-    await fs.outputFile (outputJs, originalJs);
-    await fs.outputFile (outputTs, originalTs);
-  }
+            const propertyName = toCamelCase (getRelationKey (fk.foreign_table_name, lRef));
+
+            const cols = columns.filter (c => lRef.includes (c.column_name));
+
+            if (cols.length === 0) continue;
+
+            const nullable = cols.reduce ((acc, col) => acc || col.is_nullable === "YES", false) ;
+
+            props[propertyName] = [
+              `(0, BelongsTo_1.default) ("${propertyName}", "${fk.foreign_table_schema}.${fk.foreign_table_name}", { lRef: ${refsAsString (lRef)}, rRef: ${refsAsString (rRef)} })${nullable ? `.nullable ()` : ""}`,
+              `${propertyName}: RefProp<"${propertyName}", "${fk.foreign_table_schema}.${fk.foreign_table_name}", "BelongsTo", ${nullable ? "true" : "false"}>;`
+            ];
+          }
+
+          for (const fk of intermediateForeignKeys) {
+            const [lRel, rRel] = fk;
+            if (!lRel || !rRel) continue;
+            const lRef = lRel.foreign_column_names;
+            const lxRef = lRel.column_names;
+            const xTable = `${lRel.table_schema}.${lRel.table_name}`;
+            const rxRef = rRel.column_names;
+            const rRef = rRel.foreign_column_names;
+
+            const propertyName = pluralize (toCamelCase (rRel.foreign_table_name));
+
+            props[propertyName] = [
+              `(0, BelongsToMany_1.default) ("${propertyName}", "${rRel.foreign_table_schema}.${rRel.foreign_table_name}", { lRef: ${refsAsString (lRef)}, lxRef: ${refsAsString (lxRef)}, xTable: "${xTable}", rxRef: ${refsAsString (rxRef)}, rRef: ${refsAsString (rRef)} })`,
+              `${propertyName}: RefProp<"${propertyName}", "${rRel.foreign_table_schema}.${rRel.foreign_table_name}", "BelongsToMany", false>;`
+            ];
+          }
+
+          const interfaceProperties = Object.values (props);
+
+          return [
+            `      ${tableName}: Table ("${table_schema}.${table}", [\n        ${interfaceProperties.map (([js]) => js).join (",\n        ")}\n      ])`,
+            `    ${tableName}: Table<"${table_schema}.${table}", {\n      ${interfaceProperties.map (([_js, ts]) => ts).join ("\n      ")}\n    }>;`
+          ];
+        }));
+
+      const schemaContent = [
+        `    ${schema}: {\n${interfacesNew.map (([js]) => js).join (",\n")}\n    }`,
+        `  ${schema}: {\n${interfacesNew.map (([_js, ts]) => ts).join ("\n")}\n  };`
+      ];
+
+      return schemaContent;
+    })
+  );
+
+  outputJs += schemaMap.map (([js]) => js).join (",\n");
+  outputTs += schemaMap.map (([_js, ts]) => ts).join ("\n");
+
+  // footer
+  outputJs += footerJs.join ("\n");
+  outputTs += "\n};";
+
+
+  return [outputJs, outputTs];
 }
 
 function mapPostgresTypeToPropType(pgType: string, nullable: boolean): string[] {
@@ -257,6 +244,24 @@ function mapPostgresTypeToPropType(pgType: string, nullable: boolean): string[] 
   const mapped = mapTo ();
 
   return [mapped[0], nullable ? `${mapped[1]} | null` : mapped[1]];
+}
+
+async function introspect(sql: typeof sqlX, options: RequiredRefQLOptions) {
+  const outputJsFile = `${outputDir}/index.js`;
+  const outputTsFile = `${outputDir}/index.d.ts`;
+  const originalJs = fs.readFileSync (outputJsFile, "utf-8");
+  const originalTs = fs.readFileSync (outputTsFile, "utf-8");
+
+  try {
+    const [outputJs, outputTs] = await introspectPG (sql, options);
+    await fs.outputFile (outputJsFile, outputJs);
+    await fs.outputFile (outputTsFile, outputTs);
+    console.log ("RefQL introspect completed!");
+  } catch (e) {
+    await fs.outputFile (outputJsFile, originalJs);
+    await fs.outputFile (outputTsFile, originalTs);
+    console.error (`RefQL introspect failed!`);
+  }
 }
 
 export default introspect;
